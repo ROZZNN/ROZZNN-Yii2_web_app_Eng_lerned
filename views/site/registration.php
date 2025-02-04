b819124c72e40ca8e85f23365a3d5157a750763d
ROZZNN <av5gust.slap@yandex.ru>
2025-02-04 16:57:00 +0300

    add bootstarp in registration form

--- views/site/registration.php
+++ views/site/registration.php
@@ -11,13 +11,30 @@ use yii\bootstrap5\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-        <?= $form->field($model, 'login') ?>
-        <?= $form->field($model, 'email') ?>
-        <?= $form->field($model, 'password') ?>
-    
-        <div class="form-group">
-            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
-        </div>
+    <div class="mb-3">
+        <?= $form->field($model, 'login')->textInput(['class' => 'form-control', 'aria-describedby' => 'loginHelp'])->label('Логин') ?>
+        <div id="loginHelp" class="form-text">We'll never share your login with anyone else.</div>
+    </div>
+
+
+    <div class="mb-3">
+        <?= $form->field($model, 'email')->textInput(['class' => 'form-control', 'aria-describedby' => 'emailHelp'])->label('Почта') ?>
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+
+    <div class="mb-3">
+        <?= $form->field($model, 'password')->passwordInput(['class' => 'form-control'])->label('Пароль') ?>
+    </div>
+
+    <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Я соглашаюсь с пользовательским соглашением</label>
+    </div>
+
+    <div class="form-group">
+        <?= Html::submitButton('Зарегистрироваться', ['class' => 'btn btn-primary'])?>
+    </div>
+
     <?php ActiveForm::end(); ?>
 
-</div><!-- site-registration --> 
+</div><!-- site-registration -->
